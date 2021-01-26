4e752513ef43704fc45a53cfc16af71510dd01f9
L1STOV <listovspartak99@gmail.com>
2021-01-26 14:42:21 +0200

    created functions for checking password requirements and root password + tests

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from db_config import connection, cursor
 
 
-if __name__ == '__main__':
+def set_up_root_password(password):
     pass
+
+
+def passwords_requirements_checker(password):
+    pass
+
+
+if __name__ == '__main__':
+    root_password = input('root password: ')
--- test_main.py
+++ test_main.py
@@ -1,12 +1,19 @@
 import unittest
+from main import set_up_root_password, passwords_requirements_checker
 
 
 class TestMain(unittest.TestCase):
     def setUp(self):
-        pass
+        self.root_password = 'Temp123456789#'
 
     def tearDown(self):
-        pass
+        self.root_password = ''
+
+    def test_set_up_root_password(self):
+        self.assertEqual(set_up_root_password(self.root_password), '*** root password was created ***')
+
+    def test_password_requirements_checker(self):
+        self.assertEqual(passwords_requirements_checker(self.root_password), '*** password requirements are met *** ')
 
 
 if __name__ == '__main__':
